cb2fd89f9eecb7e1bc8ea878b5eb1ab7a1cb0b61
Sarah Groves <sarahmaddoxgroves@gmail.com>
2025-02-03 16:57:55 -0500

    updates to FD and NMG

--- CahnHilliard_MATLAB_solvers/CahnHilliard_FD_SMG.m
+++ CahnHilliard_MATLAB_solvers/CahnHilliard_FD_SMG.m
@@ -1,5 +1,5 @@
-function [t_out,phi_t,delta_mass_t,E_t] = CahnHilliard_NMG_SMG(phi0,varargin)
-%This function uses the nonlinear multigrid method to solve the 
+function [t_out,phi_t,delta_mass_t,E_t] = CahnHilliard_FD_SMG(phi0,varargin)
+%This function uses the finite difference (Euler) method to solve the 
 %Cahn-Hilliard equation for a specified number of time steps of size dt.
 % 
 %INPUTS
@@ -8,8 +8,6 @@ function [t_out,phi_t,delta_mass_t,E_t] = CahnHilliard_NMG_SMG(phi0,varargin)
 %NAME-VALUE PAIRS
     %t_iter = Number of time steps simulated. Default = 1e3.
     %dt = Time step. Default = 2.5e-5 characteristic times.
-    %solver_iter = Number of solver iterations per time step. Default = 1e4.
-    %tol = Solver tolerance per time step. Default = 1e-5.
     %dt_out = Spacing of time steps output to phi_t as a multidimensional
     %   array (if less than 1e9 elements) or printed file (if greater than
     %   1e9 elements). Default = 1, to output every time step. 
@@ -19,14 +17,15 @@ function [t_out,phi_t,delta_mass_t,E_t] = CahnHilliard_NMG_SMG(phi0,varargin)
     %boundary = Boundary conditions for the simulation:
     %   'periodic' (default) - flux on one domain border equals negative flux on the opposite border.
     %   'neumann' - zero flux on the domain borders.
-    %c_relax = Number of smoothing relaxations done at the start and end
-    %   of each V-cycle. Default = 2.
+    %c_relax = Number of smoothing relaxations done at the start of the
+    %   finite difference method. Default = 2.  **DO WE WANT TO MAKE THIS THE DEFAULT**
     %domain = Vector of rightmost and leftmost grid points in x and y.
     %   Format: [xright xleft yright yleft]. Default = [1 0 1 0].
     %printres = Logical to print solver residuals to a file. Default = false.
     %printphi = Logical to print phi to a file regardless of whether or 
     %   not it can be saved as a multidimensional array. Default = false.
     % pathname = Name of the path to which phi is printed. Default = 'cd'. May include a prefix for the filename.
+    %presmooth = Optional smoothing before the solver. Default = false.
 
 %
 %OUTPUT
@@ -40,8 +39,6 @@ function [t_out,phi_t,delta_mass_t,E_t] = CahnHilliard_NMG_SMG(phi0,varargin)
 %Set parameter defaults
 default_t_iter = 1e3;
 default_dt = 2.5e-5;
-default_solver_iter = 1e4;
-default_tol = 1e-5;
 default_dt_out = 1;
 default_m = 4;
 default_epsilon2 = nan;
@@ -51,8 +48,9 @@ default_domain = [1 0 1 0];
 default_printres = false;
 default_printphi = false;
 default_pathname = 'cd';
+default_presmooth = false;
 
-CahnHilliard_NMG_parser = inputParser;
+CahnHilliard_FD_parser = inputParser;
 
 %Set general criteria for inputs and name-value pairs
 valid_matrix = @(x) ismatrix(x);
@@ -61,54 +59,47 @@ valid_pos_num = @(x) isnumeric(x) && (x > 0);
 valid_boundary_type = @(x) strcmpi(x,'periodic') || strcmpi(x,'neumann');
 valid_domain_vector = @(x) length(x) == 4;
 valid_logical = @(x) islogical(x) || x == 1 || x == 0;
-valid_string = @(x) ischar(x) || isstring(x);
 
 %Set parser options and valid input criteria
-addRequired(CahnHilliard_NMG_parser,'phi0',valid_matrix);
-addParameter(CahnHilliard_NMG_parser,'t_iter',default_t_iter,valid_integer);
-addParameter(CahnHilliard_NMG_parser,'dt',default_dt,valid_pos_num);
-addParameter(CahnHilliard_NMG_parser,'solver_iter',default_solver_iter,valid_integer);
-addParameter(CahnHilliard_NMG_parser,'tol',default_tol,valid_pos_num);
-addParameter(CahnHilliard_NMG_parser,'dt_out',default_dt_out,valid_integer);
-addParameter(CahnHilliard_NMG_parser,'m',default_m,valid_integer);
-addParameter(CahnHilliard_NMG_parser,'epsilon2',default_epsilon2,valid_pos_num);
-addParameter(CahnHilliard_NMG_parser,'boundary',default_boundary,valid_boundary_type);
-addParameter(CahnHilliard_NMG_parser,'c_relax',default_c_relax,valid_integer);
-addParameter(CahnHilliard_NMG_parser,'domain',default_domain,valid_domain_vector);
-addParameter(CahnHilliard_NMG_parser,'printres',default_printres,valid_logical);
-addParameter(CahnHilliard_NMG_parser,'printphi',default_printphi,valid_logical);
-addParameter(CahnHilliard_NMG_parser,'pathname',default_pathname,valid_string);
+addRequired(CahnHilliard_FD_parser,'phi0',valid_matrix);
+   
+addParameter(CahnHilliard_FD_parser,'t_iter',default_t_iter,valid_integer);
+addParameter(CahnHilliard_FD_parser,'dt',default_dt,valid_pos_num);
+addParameter(CahnHilliard_FD_parser,'dt_out',default_dt_out,valid_integer);
+addParameter(CahnHilliard_FD_parser,'m',default_m,valid_integer);
+addParameter(CahnHilliard_FD_parser,'epsilon2',default_epsilon2,valid_pos_num);
+addParameter(CahnHilliard_FD_parser,'boundary',default_boundary,valid_boundary_type);
+addParameter(CahnHilliard_FD_parser,'c_relax',default_c_relax,valid_integer);
+addParameter(CahnHilliard_FD_parser,'domain',default_domain,valid_domain_vector);
+addParameter(CahnHilliard_FD_parser,'printres',default_printres,valid_logical);
+addParameter(CahnHilliard_FD_parser,'printphi',default_printphi,valid_logical);
+addParameter(CahnHilliard_FD_parser,'pathname',default_pathname);
+addParameter(CahnHilliard_FD_parser,'presmooth',default_presmooth,valid_logical);
 
-parse(CahnHilliard_NMG_parser,phi0,varargin{:});
+
+parse(CahnHilliard_FD_parser,phi0,varargin{:});
+
 %Extract parsed inputs
-phi0 = CahnHilliard_NMG_parser.Results.phi0;
-t_iter = CahnHilliard_NMG_parser.Results.t_iter;
-dt = CahnHilliard_NMG_parser.Results.dt;
-solver_iter = CahnHilliard_NMG_parser.Results.solver_iter;
-tol = CahnHilliard_NMG_parser.Results.tol;
-dt_out = CahnHilliard_NMG_parser.Results.dt_out;
-m = CahnHilliard_NMG_parser.Results.m;
-epsilon2 = CahnHilliard_NMG_parser.Results.epsilon2;
-boundary = CahnHilliard_NMG_parser.Results.boundary;
-c_relax = CahnHilliard_NMG_parser.Results.c_relax;
-xright = CahnHilliard_NMG_parser.Results.domain(1);
-xleft = CahnHilliard_NMG_parser.Results.domain(2);
-yright = CahnHilliard_NMG_parser.Results.domain(3);
-yleft = CahnHilliard_NMG_parser.Results.domain(4);
-printres = CahnHilliard_NMG_parser.Results.printres;
-printphi = CahnHilliard_NMG_parser.Results.printphi;
-pathname = CahnHilliard_NMG_parser.Results.pathname;
+phi0 = CahnHilliard_FD_parser.Results.phi0;
+t_iter = CahnHilliard_FD_parser.Results.t_iter;
+dt = CahnHilliard_FD_parser.Results.dt;
+dt_out = CahnHilliard_FD_parser.Results.dt_out;
+m = CahnHilliard_FD_parser.Results.m;
+epsilon2 = CahnHilliard_FD_parser.Results.epsilon2;
+boundary = CahnHilliard_FD_parser.Results.boundary;
+c_relax = CahnHilliard_FD_parser.Results.c_relax;
+xright = CahnHilliard_FD_parser.Results.domain(1);
+xleft = CahnHilliard_FD_parser.Results.domain(2);
+yright = CahnHilliard_FD_parser.Results.domain(3);
+yleft = CahnHilliard_FD_parser.Results.domain(4);
+printres = CahnHilliard_FD_parser.Results.printres;
+printphi = CahnHilliard_FD_parser.Results.printphi;
+pathname = CahnHilliard_FD_parser.Results.pathname;
+presmooth = CahnHilliard_FD_parser.Results.presmooth;
 
 %% Define and initialize key simulation parameters
+
 [nx,ny] = size(phi0); %Define number of grid points in x and y
-%Define maximum possible V-cycle levels as the largest power of two
-%shared by both x and y dimensions
-i = 1;
-while i < min([floor(log2(nx)) floor(log2(ny))]) && mod(nx,2^i) == 0 ...
-    && mod(ny,2^i) == 0
-    i = i+1;
-end
-n_level = i; clear i;
 h2 = ((xright-xleft)/nx)*((yright-yleft)/ny); %Define mesh size
 if isnan(epsilon2)
     epsilon2 = h2*m^2/(2*sqrt(2)*atanh(0.9))^2; %Define ϵ^2 if not prespecified
@@ -116,9 +107,11 @@ else
     m = sqrt((epsilon2*(2*sqrt(2)*atanh(0.9))^2)/h2); %Else overwrite m
 end
 
-mu = zeros(nx,ny); %Initialize chemical potential
-phi_old = phi0; %Initialize prior chemical state
-phi_new = phi0; %Initialize next chemical state
+if presmooth
+    [phi_old,~] = ch_smooth(phi0,zeros(nx,ny),phi0/dt- ...
+    ch_laplace(phi0,nx,ny,xright,xleft,yright,yleft,boundary),zeros(nx,ny), ...
+    c_relax,nx,ny,xright,xleft,yright,yleft,dt,epsilon2,boundary); %Smooth phi0 c_relax times
+end
 downsampled = nx*ny*t_iter/dt_out > 1e9; %Logical index for the need to downsample
 n_timesteps = floor(t_iter/dt_out);
 if printphi %print to file
@@ -149,14 +142,14 @@ end
 mass_t(1) = sum(sum(phi0))/(h2*nx*ny);
 E_t(1) = ch_discrete_energy(phi0,h2,epsilon2);
 
-%% Run nonlinear multigrid solver
+%% Run finite difference solver
+
 if printres
     fprintf('Saving squared residuals per iteration to file in the working directory\n')
 end
 for i = 1:t_iter
-    phi_new = nmg_solver(phi_old,phi_new,mu,nx,ny, ...
-        xright,xleft,yright,yleft,c_relax,dt,epsilon2,n_level, ...
-        solver_iter,tol,boundary,printres);
+    phi_new = fd_solver(phi_old,nx,ny,xright,xleft,yright,yleft, ...
+        dt,epsilon2,boundary,printres);
     phi_old = phi_new;
     if mod(i/t_iter*100,5) == 0
         fprintf('%3.0f percent complete\n',i/t_iter*100)
@@ -178,4 +171,5 @@ end
 %Center mass and normalize energy to t == 0
 delta_mass_t = mass_t - mass_t(1);
 E_t = E_t/E_t(1);
+
 end
