bc3f3f2b7be4d3c2925f8c64c8199b49054de596
Sarah Groves <sarahmaddoxgroves@gmail.com>
2025-04-22 16:31:52 -0400

    additional runs from rivanna

--- plot_job_specs.py
+++ plot_job_specs.py
@@ -14,10 +14,11 @@ data['comp'] = "local"
 data_rivanna['comp'] = "rivanna"
 
 # %%
-for i, r in data_rivanna.loc[data_rivanna['t_iter'] == 20].iterrows():
+length = 20
+for i, r in data_rivanna.loc[data_rivanna['t_iter'] == length].iterrows():
     new_r = r.copy()
-    new_r['t_iter'] = new_r['t_iter'] * 100
-    new_r['elapsed_time(s)'] = new_r['elapsed_time(s)'] * 100
+    new_r['t_iter'] = new_r['t_iter'] * 2000//length
+    new_r['elapsed_time(s)'] = new_r['elapsed_time(s)'] * 2000//length
     new_r['comp'] = "rivanna_extrap"
     data_rivanna.loc[data_rivanna.shape[0]+1] = new_r
 
@@ -25,20 +26,21 @@ for i, r in data_rivanna.loc[data_rivanna['t_iter'] == 20].iterrows():
 all_data = pd.concat([data, data_rivanna], join='outer', ignore_index=True)
 # %%
 all_data = all_data.loc[all_data["t_iter"] == 2000]
-all_data = all_data.loc[all_data['solver'] != "FD"]
+all_data = all_data.loc[all_data['method'] != "FD"]
+all_data = all_data.loc[all_data['comp'] != "local"]
 
 # %%
-all_data = all_data.loc[~((all_data["language"] == "Julia")
-                          & (all_data["comp"] == "local"))]
+# all_data = all_data.loc[~((all_data["language"] == "Julia")
+#   & (all_data["comp"] == "local"))]
 # %%
-bc = "periodic"
-GridSize = 512
+bc = "neumann"
+GridSize = 128
 g = sns.catplot(
     all_data.loc[(all_data["GridSize"] == GridSize) &
                  (all_data["boundary"] == bc)],
     kind="bar",
     y="elapsed_time(s)",
-    x="solver",
+    x="method",
     # hue="bc",
     col="language",
     height=4,
@@ -56,13 +58,41 @@ plt.tight_layout()
 plt.show()
 # plt.savefig(f"./output/compare_runtime_{bc}_{GridSize}.pdf")
 # %%
-sns.lineplot(
-    data,
-    y="time (secs)",
-    x="nx",
-    hue="solver",
+# %%
+bc = "periodic"
+GridSize = 128
+g = sns.catplot(
+    data_rivanna.loc[(data_rivanna["GridSize"] == GridSize) &
+                     (data_rivanna["boundary"] == bc)],
+    kind="bar",
+    y="elapsed_time(s)",
+    x="method",
+    # hue="bc",
+    col="language",
+    height=4,
+    aspect=0.5,
+    col_order=["Python", "MATLAB", "Julia"],
+    # log=True
+    # palette="Set2"
+)
+plt.ylim(1, 1e6)
+g.figure.get_axes()[0].set_yscale('log')
+plt.suptitle(
+    f"Elapsed Time for {GridSize}x{GridSize} Grid Size, 2000 Iterations, {bc.capitalize()} BC", y=1.)
+g.set_axis_labels("Solver", "Elapsed Time (log[sec])")
+plt.tight_layout()
+plt.show()
+# %%
+g = sns.lineplot(
+    all_data,
+    y="elapsed_time(s)",
+    x="GridSize",
+    hue="method",
     style="language",
 )
+g.figure.get_axes()[0].set_yscale('log')
+g.figure.get_axes()[0].set_xscale('log')
+
 plt.show()
 
 # %%
@@ -83,6 +113,7 @@ g = sns.catplot(
 )
 # plt.ylim(1, 1e7)
 g.figure.get_axes()[0].set_yscale('log')
+
 plt.suptitle(
     f"Memory Allocated for 2000 Iterations, {bc.capitalize()} BC, Julia", y=1.)
 g.set_axis_labels("Solver", "Memory Allocated (MB)")
