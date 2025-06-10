286b6b2e704a48bc21f241d0aa808035717951a6
Sarah Groves <sarahmaddoxgroves@gmail.com>
2025-06-10 16:52:13 -0400

    updated plots with fixed periodic NMG code

--- CahnHilliard_MATLAB_solvers/run_spinodal_decomp.m
+++ CahnHilliard_MATLAB_solvers/run_spinodal_decomp.m
@@ -19,27 +19,27 @@ ny = GridSize;
 % RUN SAV SOLVER 
 % #################################################
 
-pathname = sprintf("%s/SAV_MATLAB_%d_dt_%.2e_Nx_%d_n_relax_%d_dtout_1_",outdir,max_it,dt, GridSize, n_relax);
-fprintf("Running SAV solver with parameters: %s\n", pathname);
-tStart_SAV = tic;
-[t_out, phi_t, delta_mass_t, E_t] = CahnHilliard_SAV(phi0,...
-                                    t_iter = max_it,...
-                                    dt = dt,...
-                                    m = m,...
-                                    boundary = boundary,...
-                                    printphi=print_phi,...
-                                    pathname=pathname,...
-                                    dt_out = dt_out);
-elapsedTime = toc(tStart_SAV);
+% pathname = sprintf("%s/SAV_MATLAB_%d_dt_%.2e_Nx_%d_n_relax_%d_dtout_1_",outdir,max_it,dt, GridSize, n_relax);
+% fprintf("Running SAV solver with parameters: %s\n", pathname);
+% tStart_SAV = tic;
+% [t_out, phi_t, delta_mass_t, E_t] = CahnHilliard_SAV(phi0,...
+%                                     t_iter = max_it,...
+%                                     dt = dt,...
+%                                     m = m,...
+%                                     boundary = boundary,...
+%                                     printphi=print_phi,...
+%                                     pathname=pathname,...
+%                                     dt_out = dt_out);
+% elapsedTime = toc(tStart_SAV);
 
-fid = fopen('../Job_specs.csv', 'a+');
-v = [string(datetime) "SAV_spinodal_decomp_smoothed_periodic_dtout_10_relaxation" "MATLAB" "SAV" GridSize epsilon dt 'NaN' max_it 'NaN' pathname elapsedTime "NaN" boundary];
-fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
-fclose(fid);
+% fid = fopen('../Job_specs.csv', 'a+');
+% v = [string(datetime) "SAV_spinodal_decomp_smoothed_periodic_dtout_10_relaxation" "MATLAB" "SAV" GridSize epsilon dt 'NaN' max_it 'NaN' pathname elapsedTime "NaN" boundary];
+% fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
+% fclose(fid);
 
-% writematrix(phi_t(:,:,end),sprintf('%sfinal_phi.csv', pathname));
-writematrix(delta_mass_t,sprintf('%smass_uncentered.csv', pathname));
-writematrix(E_t,sprintf('%senergy.csv', pathname));
+% % writematrix(phi_t(:,:,end),sprintf('%sfinal_phi.csv', pathname));
+% writematrix(delta_mass_t,sprintf('%smass_uncentered.csv', pathname));
+% writematrix(E_t,sprintf('%senergy.csv', pathname));
 
 % fprintf("Creating movie\n");
 % filename = strcat(pathname, "movie");
@@ -53,7 +53,7 @@ writematrix(E_t,sprintf('%senergy.csv', pathname));
 % % RUN NMG SOLVER 
 % % #################################################
 
-pathname = sprintf("%s/NMG_MATLAB_%d_dt_%.2e_Nx_%d_n_relax_%d_dtout_1_",outdir,max_it,dt, GridSize, n_relax);
+pathname = sprintf("%s/NMG_MATLAB_%d_dt_%.2e_Nx_%d_n_relax_%d_dtout_1_tol_1e-6",outdir,max_it,dt, GridSize, n_relax);
 fprintf("Running NMG solver with parameters: %s\n", pathname);
 tStart_NMG = tic;
 [t_out, phi_t, delta_mass_t, E_t] = CahnHilliard_NMG(phi0,...
@@ -63,13 +63,14 @@ tStart_NMG = tic;
                                     boundary = boundary,...
                                     printphi=print_phi,...
                                     pathname=pathname,...
-                                    dt_out = dt_out);
+                                    dt_out = dt_out,...
+                                    tol = 1e-6);
 elapsedTime = toc(tStart_NMG);
 
-fid = fopen('../Job_specs.csv', 'a+');
-v = [string(datetime) "NMG_spinodal_decomp_smoothed_dtout_10" "MATLAB" "NMG" GridSize epsilon dt 'NaN' max_it  pathname elapsedTime 'NaN' "periodic"];
-fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
-fclose(fid);
+% fid = fopen('../Job_specs.csv', 'a+');
+% v = [string(datetime) "NMG_spinodal_decomp_smoothed_dtout_10" "MATLAB" "NMG" GridSize epsilon dt 'NaN' max_it  pathname elapsedTime 'NaN' "periodic"];
+% fprintf(fid, '%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n', v);
+% fclose(fid);
 
 % writematrix(phi_t(:,:,end),sprintf('%sfinal_phi.csv', pathname));
 writematrix(delta_mass_t,sprintf('%smass_uncentered.csv', pathname));
